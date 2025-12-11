552d1324b3c8f1e0b1c40a95a78bade5d9c3cd74
Tri1606 <thientri.nguyen2020@gmail.com>
2025-12-10 22:27:53 -0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,136 @@
+function changeAlbum1() {
+  document.getElementById("Text1").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "red-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box white center-content clickable8");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/9kaCAbIXuyg?si=mc1mqGj_csVAJCPE";
+  document.getElementById("Text").innerHTML =
+    "Untitled, 2014 is an emotional piano ballad by G-Dragon that expresses deep regret, sorrow, and longing for a past love. The song features a minimalist arrangement mainly just piano and vocals, which helps highlight the raw, vulnerable emotions in his voice. The lyrics reflect a heartfelt apology and a desire for reconciliation, even if it means just saying goodbye properly. Its soft, melancholic, and sincere, showing a more vulnerable and mature side of G-Dragon. (Click to reset)";
+}
+function changeAlbum2() {
+  document.getElementById("Text2").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "white-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box pink center-content clickable9");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/d9IxdwEFk1c?si=F89xCdGYvSlHfPoo";
+  document.getElementById("Text").innerHTML =
+    "Palette is a smooth pop-R&B song by IU featuring G-Dragon that reflects self-discovery, maturity, and confidence in growing older. IU sings about learning what she truly likes her style, colors, personality and becoming more comfortable with herself at age 25. The song has a calm, comforting vibe with soft vocals and a dreamy melody. G-Dragons rap adds a thoughtful and supportive perspective, praising her growth and individuality. Overall, its a warm, reflective song about understanding and accepting oneself while entering adulthood. (Click to reset)";
+}
+function changeAlbum3() {
+  document.getElementById("Text3").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "turqoise-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box black center-content clickable10");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/o9DhvbqYzns?si=XATHUd3nasAl9mwB-";
+  document.getElementById("Text").innerHTML =
+    "Too Bad is a groovy R&B and hip-hop fusion track by G-Dragon featuring Anderson .Paak. The song carries a smooth, laid-back vibe with a funky rhythm, soulful vocals, and charismatic rap verses. Lyrically, it talks about a complicated relationship—two people who feel a strong attraction but are too different or too late to make it work. Anderson .Paak brings a playful yet emotional tone, while G-Dragon adds style and attitude with his rap and melodic flow. The song blends Western and Korean musical styles, creating a cool, modern, and stylish mood. (Click to reset)";
+}
+function changeLightstick1() {
+  document.getElementById("Text8").classList.add("press-down");
+  document.getElementById("video").setAttribute("class", "videosize gone");
+  document.getElementById("photo").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "yellow-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box black center-content clickable12");
+  document.getElementById("Text").innerHTML =
+    "The Daisy Lightstick, inspired by G-Dragons Peaceminusone daisy logo, features a white flower head with a bright yellow center on a sleek black handle with minimalist branding. It represents peace, individuality, and G-Dragons signature artistic and fashion-forward identity. When lit, it gives off a soft, warm glow, creating a calm and aesthetic ambiance rather than a flashy concert light. Simple, stylish, and symbolic, its cherished by fans not only as a lightstick but also as a unique collectible and design piece that captures G-Dragons creative spirit. (Click to reset)";
+  document.getElementById("photo").src = "Images/photo1.png";
+}
+function changeLightstick2() {
+  document.getElementById("Text9").classList.add("press-down");
+  document.getElementById("video").setAttribute("class", "videosize gone");
+  document.getElementById("photo").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "yellow-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box black center-content clickable12");
+  document.getElementById("Text").innerHTML =
+    "The BIGBANG lightstick, known as the Bang Bong, is one of K-pops most iconic lightsticks and was originally invented and designed by G-Dragon. It features a bold yellow crown-shaped top symbolizing BIGBANGs title as the Kings of K-Pop, placed on a sleek black or white handle with the groups logo. Representing power, royalty, and VIP fandom pride, it lights up in BIGBANGs signature yellow, creating a sea of glowing crowns at concerts. Bold, stylish, and instantly recognizable, it reflects BIGBANGs legacy and strong connection with their fans. (Click to reset)";
+  document.getElementById("photo").src = "Images/photo2.jpg";
+}
+function changeAlbum4() {
+  document.getElementById("Text4").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "black-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box brown center-content clickable11");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/9bKSjRJhXwo?si=B0ufF2n4FnQYMjlP";
+  document.getElementById("Text").innerHTML =
+    "Without You is an emotional pop-ballad by G-Dragon featuring Roses delicate vocals. The song expresses the pain of separation and the empty feeling when someone you loved is no longer in your life. It has a soft, melancholic melody blending piano, strings, and gentle beats. G-Dragons verses show heartbreak and regret, while Roses airy voice adds warmth and sadness, echoing the feeling of longing. The song captures the message that even though life goes on, it feels incomplete without that person. (Click to reset)";
+}
+function changeAlbum5() {
+  document.getElementById("Text5").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "black-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box dark-red center-content clickable11");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/RKhsHGfrFmY?si=srno3oiG0WbxDsOp";
+  document.getElementById("Text").innerHTML =
+    "Crooked by G-Dragon is a high-energy rock-influenced track that reveals the pain behind his rebellious image. Beneath its upbeat tempo and punk-style rhythm, the song expresses feelings of loneliness, frustration, and emotional isolation. G-Dragon uses crooked as a metaphor for wanting to act tough and carefree while secretly yearning for understanding and love. Its raw honesty, catchy hook, and intense emotion made Crooked one of the standout and most popular songs from his album Coup Detat, showcasing both his musical versatility and vulnerability. (Click to reset)";
+}
+
+function changeAlbum6() {
+  document.getElementById("Text6").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "white-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box black center-content clickable9");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/2ips2mM7Zqw?si=b33NDRZmBKgUcPQ2";
+  document.getElementById("Text").innerHTML =
+    "Bang Bang Bang is a high-energy EDM and hip-hop track by BIGBANG known for its explosive beat, powerful chorus, and intense party vibe. The song features bold rap verses, dynamic vocals, and a dramatic build-up that drops into a loud, festival-style hook. Lyrically, its all about confidence, excitement, and dominating the moment—like a wild anthem made for dancing and hyping up a crowd. With its catchy chants and aggressive rhythm, its one of BIGBANG most iconic and electrifying songs. (Click to reset)";
+}
+function changeAlbum7() {
+  document.getElementById("Text7").classList.add("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("video").setAttribute("class", "videosize");
+  document.getElementById("Text").setAttribute("class", "blue-text");
+  document
+    .getElementById("box")
+    .setAttribute("class", "box gray center-content clickable13");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/AAbokV76tkU?si=LvzlVrj32-S6T0T5";
+  document.getElementById("Text").innerHTML =
+    "Fantastic Baby is an energetic EDM and dance-pop anthem by BIGBANG, famous for its explosive beat, iconic Wow, fantastic baby hook, and electrifying party atmosphere. The song celebrates freedom, excitement, and breaking rules, encouraging listeners to let loose and express themselves. With powerful vocals, bold rap parts, and a dynamic club-style drop, it creates a wild, festival-like vibe. Its one of BIGBANGs most iconic tracks, known for its hype, rebellious spirit, and unforgettable chorus. (Click to reset)";
+  document.getElementById("my-click").play();
+}
+function reset() {
+  document.getElementById("Text1").classList.remove("press-down");
+  document.getElementById("Text2").classList.remove("press-down");
+  document.getElementById("Text3").classList.remove("press-down");
+  document.getElementById("Text4").classList.remove("press-down");
+  document.getElementById("Text5").classList.remove("press-down");
+  document.getElementById("Text6").classList.remove("press-down");
+  document.getElementById("Text7").classList.remove("press-down");
+  document.getElementById("Text8").classList.remove("press-down");
+  document.getElementById("Text9").classList.remove("press-down");
+  document.getElementById("photo").setAttribute("class", "videosize gone");
+  document.getElementById("Text").innerHTML = "";
+  document.getElementById("box").setAttribute("class", "box center-content");
+  document.getElementById("video").src =
+    "https://www.youtube-nocookie.com/embed/LpsUxEDWpIw?si=Lv_MWQgKAZJnaV9y";
+}
+function playSound() {
+  document.getElementById("my-click").play();
+}
